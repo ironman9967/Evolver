912203d31959726498223ff0318a43b57b7b469a
Tyson Hester <ironman9967@gmail.com>
2014-03-16 15:59:09 -0400

    .

--- Tests/test.js
+++ Tests/test.js
@@ -9,10 +9,12 @@ var Genome = require('../Classes/DNA/Objects/Genome');
 var Organism = require('../Classes/Evolution/Organism');
 
 var testName = "Heat Tolerance";
-var goal = 10;
+var goal = 50;
+var lifeSpan = 10;
+var numOfOrganisms = 10000;
 
 var basePairDefinition1 = new BasePairDefinition(testName);
-basePairDefinition1.Elements = _.range(5, 16);
+basePairDefinition1.Elements = _.range(1, 101);
 
 var geneDefinition1 = new GeneDefinition(testName);
 geneDefinition1.BasePairDefinitions.push(basePairDefinition1);
@@ -27,42 +29,40 @@ chromosomeDefinition1.GeneDefinitions.push(geneDefinition1);
 var genomeDefinition1 = new GenomeDefinition(testName);
 genomeDefinition1.ChromosomeDefinitions.push(chromosomeDefinition1);
 
-var numOfOrganisms = 250;
 var organisms = [];
 
 for (var i = 0; i < numOfOrganisms; i++) {
 	organisms.push(new Organism(new Genome(genomeDefinition1, true)));
 }
 
-var lifeSpan = 10;
 
 console.log("STARTING FITNESS TEST: " + testName);
 console.log("-- population size: " + numOfOrganisms);
 console.log("-- goal is to have " + testName + " of " + goal);
 console.log("-- breed cycle/lifespan: ~" + lifeSpan + "ms");
 
-setInterval(function () {
-	organisms.pop();
-}, lifeSpan);
+setTimeout(function () {
+    setInterval(function () {
+        organisms.pop();
+    }, lifeSpan);
 
-setInterval(function () {
-	organisms.push(organisms[0].Breed(organisms[1]));
-	organisms = _.shuffle(organisms);
-}, lifeSpan);
+    setInterval(function () {
+        organisms.push(organisms[0].Breed(organisms[1]));
+        organisms = _.shuffle(organisms);
+    }, lifeSpan);
 
-var stamp = (new Date()).getTime();
-var interval = setInterval(function () {
-	var min = _.min(organisms, function (o) {
-		return o.Genome.Chromosomes[testName].Genes[testName].Phenotype;
-	}).Genome.Chromosomes[testName].Genes[testName].Phenotype;
-	var max = _.max(organisms, function (o) {
-		return o.Genome.Chromosomes[testName].Genes[testName].Phenotype;
-	}).Genome.Chromosomes[testName].Genes[testName].Phenotype;
-	console.log(testName + " population min: " + min + " --- " + testName + " population max: " + max);
-	if (min === 10 && max === 10) {
-		console.log("ALL ORGANISMS ARE PERFECT: ~" + Math.floor(((new Date()).getTime() - stamp) / 1000) + " seconds");
-		clearInterval(interval);
-	}
-}, lifeSpan * 50);
-
-
+    var stamp = (new Date()).getTime();
+    var interval = setInterval(function () {
+        var min = _.min(organisms,function (o) {
+            return o.Genome.Chromosomes[testName].Genes[testName].Phenotype;
+        }).Genome.Chromosomes[testName].Genes[testName].Phenotype;
+        var max = _.max(organisms,function (o) {
+            return o.Genome.Chromosomes[testName].Genes[testName].Phenotype;
+        }).Genome.Chromosomes[testName].Genes[testName].Phenotype;
+        console.log(testName + " population min: " + min + " --- " + testName + " population max: " + max);
+        if (min === goal && max === goal) {
+            console.log("ALL ORGANISMS ARE PERFECT: ~" + Math.floor(((new Date()).getTime() - stamp) / 1000) + " seconds");
+            clearInterval(interval);
+        }
+    }, lifeSpan * 50);
+}, 6000);
